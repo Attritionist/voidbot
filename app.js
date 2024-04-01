ae0cb66ce661c816b8927fe74d5911365a66e15b
Attritionist <55213784+Attritionist@users.noreply.github.com>
2024-04-01 20:46:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -175,7 +175,7 @@ async function detectUniswapLatestTransaction() {
       const addressLink = `https://debank.com/profile/${AddressOf}`;
       const txHashLink = `https://basescan.org/tx/${transaction.hash}`;
       const chartLink =
-        "https://dexscreener.com/base/0xBf949F74Eb6Ae999f35e4706A236f8792b88Cb73";
+        "https://dexscreener.com/base/0xb14e941d34d61ae251ccc08ac15b8455ae9f60a5";
 
       const txDetailsUrl = `https://api.basescan.org/api?module=account&action=txlistinternal&txhash=${transaction.hash}&apikey=${ETHERSCAN_API_KEY}`;
       const amountTransferred =
