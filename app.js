3d4c507a155e7e805736df801fe858463c3eb32c
Attritionist <55213784+Attritionist@users.noreply.github.com>
2024-04-01 09:09:26 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ const ETHERSCAN_API_KEY = process.env["ETHERSCAN_API_KEY"];
 const TOKEN_CONTRACT = process.env["TOKEN_CONTRACT"];
 const POOL_CONTRACT = process.env["POOL_CONTRACT"];
 const MAX_CONSECUTIVE_NO_TRANSACTIONS = 5;
-const VOID_SLEEP_DURATION = 10000;
-const BURN_SLEEP_DURATION = 30000;
-
-let consecutiveNoTransactions = 0;
 let consecutiveNoBurn = 0;
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, { polling: true });
 const tokenDecimals = 18;
 const initialSupply = 100000000;
+const BURN_SLEEP_DURATION = 5000;
 const burnAnimation = "https://voidonbase.com/burn.gif";
-const voidAnimation = "https://voidonbase.com/void.gif";
 const fs = require("fs");
 const processedTransactionsFilePath = "processed_transactions.json";
 let processedTransactions = new Set();
@@ -64,7 +60,7 @@ setInterval(async () => {
   if (ethUsdPrice !== null) {
     currentEthUsdPrice = ethUsdPrice;
   }
-}, 20000);
+}, 5000);
 
 let currentEthUsdPrice = null;
 const messageQueue = [];
@@ -73,8 +69,10 @@ let isSendingMessage = false;
 function addToMessageQueue(message) {
   messageQueue.push(message);
 }
-
-async function sendMessageFromQueue() {
+function addToBurnQueue(message) {
+  messageQueue.push(message);
+}
+async function sendBurnFromQueue() {
   if (messageQueue.length > 0 && !isSendingMessage) {
     isSendingMessage = true;
     const message = messageQueue.shift();
@@ -95,10 +93,47 @@ async function sendMessageFromQueue() {
     }, 2500);
   }
 }
+async function sendMessageFromQueue() {
+  if (messageQueue.length > 0 && !isSendingMessage) {
+    isSendingMessage = true;
+    const message = messageQueue.shift();
+    try {
+      // Send the message
+      await bot.sendPhoto(
+        TELEGRAM_CHAT_ID,
+        message.photo,
+        message.options
+      );
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+    // Wait for 5 seconds before sending the next message
+    setTimeout(() => {
+      isSendingMessage = false;
+      sendMessageFromQueue(); // Send the next message in the queue
+    }, 2500);
+  }
+}
 
+async function sendPhotoMessage(photo, options, pinMessage = false) {
+  addToMessageQueue({ photo, options });
+sendMessageFromQueue();
+  if (pinMessage) {
+    try {
+      // Wait for a short duration to ensure the message is sent before pinning
+      await sleep(1000);
+      // Pin the message in the group
+      await bot.pinChatMessage(TELEGRAM_CHAT_ID, options.message_id, {
+        disable_notification: true 
+      });
+    } catch (error) {
+      console.error("Error pinning message:", error);
+    }
+  }
+}
 async function sendAnimationMessage(animation, options, pinMessage = false) {
-  addToMessageQueue({ animation, options });
-  sendMessageFromQueue();
+  addToBurnQueue({ animation, options });
+  sendBurnFromQueue();
 
   if (pinMessage) {
     try {
@@ -116,11 +151,6 @@ async function sendAnimationMessage(animation, options, pinMessage = false) {
 
 async function detectUniswapTransactions() {
   try {
-    if (consecutiveNoTransactions >= MAX_CONSECUTIVE_NO_TRANSACTIONS) {
-    console.log(`No new transactions detected. Sleeping for ${VOID_SLEEP_DURATION / 1000} seconds...`);
-    await sleep(VOID_SLEEP_DURATION);
-    consecutiveNoTransactions = 0; // Reset the counter after waking up
-  }
     if (currentEthUsdPrice === null) {
       console.log("Waiting for ETH-USD price data...");
       return;
@@ -139,12 +169,6 @@ async function detectUniswapTransactions() {
       (transaction) => !processedTransactions.has(transaction.hash)
     );
 
-    if (newTransactions.length === 0) {
-      console.log("No new transactions detected.");
-      consecutiveNoTransactions++;
-      return;
-    }
-    consecutiveNoTransactions = 0; // Reset the counter if transactions are found
     for (const transaction of newTransactions) {
       const amountTransferred =
         Number(transaction.value) / 10 ** tokenDecimals;
@@ -168,9 +192,7 @@ async function detectUniswapTransactions() {
           "VOID Initiate": 1000,
           "VOID Rookie": 2000,
           "VOID Novice": 3000,
-          "VOID Aspirant": 4000,
           "VOID Learner": 5000,
-          "VOID Student": 6000,
           "VOID Follower": 7000,
           "VOID Apprentice": 8000,
           "VOID Adept": 9000,
@@ -181,10 +203,8 @@ async function detectUniswapTransactions() {
           "VOID Grandmaster": 20000,
           "VOID Summoner": 22500,
           "VOID Necromancer": 25000,
-          "VOID Priest": 27500,
-          "VOID Seer": 30000,
-          "VOID Enchanter": 32500,
-          "VOID Rogue": 35000,
+          "VOID Seer": 27500,
+          "VOID Enchanter": 30000,
           "VOID Sage": 40000,
           "VOID Shaman": 45000,
           "VOID Knight": 50000,
@@ -197,9 +217,7 @@ async function detectUniswapTransactions() {
           "VOID Clairvoyant": 85000,
           "VOID Alchemist": 95000,
           "VOID Lord": 100000,
-          "VOID Paladin": 110000,
           "VOID Hierophant": 120000,
-          "VOID Prophet": 130000,
           "VOID Creature": 140000,
           "VOID Overlord": 150000,
           "VOID Emperor": 175000,
@@ -252,7 +270,159 @@ async function detectUniswapTransactions() {
               break;
             }
           }
-
+          let imageUrl = "";
+          switch (voidRank) {
+            case "VOID Peasant":
+              imageUrl = "https://voidonbase.com/rank1.png";
+              break;
+            case "VOID Initiate":
+              imageUrl = "https://voidonbase.com/rank2.png";
+              break;
+            case "VOID Learner":
+              imageUrl = "https://voidonbase.com/rank3.png";
+              break;
+            case "VOID Rookie":
+              imageUrl = "https://voidonbase.com/rank4.png";
+              break;
+            case "VOID Apprentice":
+              imageUrl = "https://voidonbase.com/rank5.png";
+              break;
+            case "VOID Expert":
+              imageUrl = "https://voidonbase.com/rank6.png";
+              break;
+            case "VOID Acolyte":
+              imageUrl = "https://voidonbase.com/rank10.png";
+              break;
+            case "VOID Disciple":
+              imageUrl = "https://voidonbase.com/rank11.png";
+              break;
+            case "VOID Master":
+              imageUrl = "https://voidonbase.com/rank12.png";
+              break;
+            case "VOID Summoner":
+              imageUrl = "https://voidonbase.com/rank14.png";
+              break;
+            case "VOID Necromancer":
+              imageUrl = "https://voidonbase.com/rank15.png";
+              break;
+            case "VOID Seer":
+              imageUrl = "https://voidonbase.com/rank16.png";
+              break;
+            case "VOID Enchanter":
+              imageUrl = "https://voidonbase.com/rank17.png";
+              break;
+              case "VOID Warrior":
+              imageUrl = "https://voidonbase.com/rankwar.png";
+              break;
+            case "VOID Sage":
+              imageUrl = "https://voidonbase.com/rank18.png";
+              break;
+            case "VOID Shaman":
+              imageUrl = "https://voidonbase.com/rank19.png";
+              break;
+            case "VOID Knight":
+              imageUrl = "https://voidonbase.com/rank20.png";
+              break;
+            case "VOID Sorcerer":
+              imageUrl = "https://voidonbase.com/rank21.png";
+              break;
+            case "VOID Warlock":
+              imageUrl = "https://voidonbase.com/rank22.png";
+              break;
+            case "VOID Archmage":
+              imageUrl = "https://voidonbase.com/rank24.png";
+              break;
+            case "VOID Archdruid":
+              imageUrl = "https://voidonbase.com/rank25.png";
+              break;
+            case "VOID Conjurer":
+              imageUrl = "https://voidonbase.com/rank26.png";
+              break;
+            case "VOID Clairvoyant":
+              imageUrl = "https://voidonbase.com/rank27.png";
+              break;
+            case "VOID Alchemist":
+              imageUrl = "https://voidonbase.com/rank28.png";
+              break;
+            case "VOID Lord":
+              imageUrl = "https://voidonbase.com/rank29.png";
+              break;
+              case "VOID Grandmaster":
+              imageUrl = "https://voidonbase.com/rankgm.png";
+              break;
+              case "VOID Juggernaut":
+              imageUrl = "https://voidonbase.com/rankjug.png";
+              break;
+            case "VOID Hierophant":
+              imageUrl = "https://voidonbase.com/rank30.png";
+              break;
+            case "VOID Creature":
+              imageUrl = "https://voidonbase.com/rank32.png";
+              break;
+            case "VOID Overlord":
+              imageUrl = "https://voidonbase.com/rank33.png";
+              break;
+            case "VOID Emperor":
+              imageUrl = "https://voidonbase.com/rank34.png";
+              break;
+            case "VOID Evoker":
+              imageUrl = "https://voidonbase.com/rank35.png";
+              break;
+            case "VOID Harbinger":
+              imageUrl = "https://voidonbase.com/rank36.png";
+              break;
+            case "VOID Warden":
+              imageUrl = "https://voidonbase.com/rank39.png";
+              break;
+            case "VOID Admiral":
+              imageUrl = "https://voidonbase.com/rank40.png";
+              break;
+            case "VOID Monarch":
+              imageUrl = "https://voidonbase.com/rank41.png";
+              break;
+            case "VOID Sovereign":
+              imageUrl = "https://voidonbase.com/rank42.png";
+              break;
+            case "VOID Majesty":
+              imageUrl = "https://voidonbase.com/rank43.png";
+              break;
+            case "VOID Transcendent":
+              imageUrl = "https://voidonbase.com/rank44.png";
+              break;
+            case "VOID Exalted":
+              imageUrl = "https://voidonbase.com/rank45.png";
+              break;
+            case "VOID Celestial":
+              imageUrl = "https://voidonbase.com/rank46.png";
+              break;
+            case "VOID Divine":
+              imageUrl = "https://voidonbase.com/rank47.png";
+              break;
+            case "VOID Apotheosis":
+              imageUrl = "https://voidonbase.com/rank48.png";
+              break;
+            case "VOID Eternity":
+              imageUrl = "https://voidonbase.com/rank49.png";
+              break;
+            case "VOID Omnipotence":
+              imageUrl = "https://voidonbase.com/rank50.png";
+              break;
+            case "VOID Singularity":
+              imageUrl = "https://voidonbase.com/rank51.png";
+              break;
+            case "VOID Absolute":
+              imageUrl = "https://voidonbase.com/rank52.png";
+              break;
+            case "VOID Omega":
+              imageUrl = "https://voidonbase.com/rank53.png";
+              break;
+            case "THE VOID":
+              imageUrl = "https://voidonbase.com/rank54.png";
+              break;
+            default:
+              imageUrl = "https://voidonbase.com/default.png"; // Default image URL if rank not found
+              break;
+          }
           const message = `${emojiString}\n\n💸 ${
             isBuy ? "Spent" : "Received"
           }: ${ethValue} ${isBuy ? "WETH" : "ETH"} ($${dollarValue})\n💼 ${
@@ -261,12 +431,12 @@ async function detectUniswapTransactions() {
               : `Sold ${amountTransferred.toFixed(3)} VOID (<a href="${addressLink}">View Address</a>)`
           }\n<a href="${chartLink}">📈 Chart</a>\n<a href="${txHashLink}">TX Hash</a>\n💰 Market Cap: $${marketCap.toLocaleString()}\n🟣 Remaining VOID Balance: ${voidBalance}\n🛡️ VOID Rank: ${voidRank}`;
 
-          const voidanimationMessageOptions = {
+          const voidMessageOptions = {
             caption: message,
             parse_mode: "HTML",
           };
-          sendAnimationMessage(voidAnimation, voidanimationMessageOptions, false);
-
+        
+          sendPhotoMessage(imageUrl, voidMessageOptions, false);
 
            processedTransactions.add(transaction.hash);
         } else {
